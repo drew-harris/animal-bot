fa1fdd02cab53cd1d579039cab372a042d8a4fb8
Drew Harris <harrisd@smu.edu>
2024-10-31 22:01:24 -0500

    feat: add generateCommand to commandsList

--- src/command/list.ts
+++ src/command/list.ts
@@ -1,7 +1,8 @@
 import { Command, OptionCommand } from ".";
+import { generateCommand } from "../commands/generateCommand";
 import { testCommand } from "../commands/testCommand";
 
-export const commandsList: Command<any>[] = [testCommand];
+export const commandsList: Command<any>[] = [testCommand, generateCommand];
 
 export const addCommand = <T>(command: OptionCommand<T>) => {
   commandsList.push(command);
