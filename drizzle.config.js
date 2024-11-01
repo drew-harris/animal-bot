e874ae920e692e16bad402c6e32c5f6846e17f4d
Drew Harris <harrisd@smu.edu>
2024-10-31 21:52:53 -0500

    fix: update drizzle config to use connection string

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,10 +2,6 @@ exports.default = {
   schema: "./src/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
+    connectionString: process.env.DB_URL,
   },
 };
